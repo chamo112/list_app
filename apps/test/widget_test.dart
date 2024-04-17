50d1869fb05a0c3fa6db922e15b68dede12519f3
chamo112 <chamo.011270@gmail.com>
2024-04-17 20:06:00 +0900

    refactor: ディレクトリ構成の変更

--- apps/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    testWidgets('blank test', (WidgetTester tester) async {
-      expect(1 == 1, true);
-    });
-  });
-}
