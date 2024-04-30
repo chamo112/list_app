a30aae36442dee15781c1f3e64b1a42ddbbbe874
chamo112 <chamo.011270@gmail.com>
2024-04-30 16:19:52 +0900

    update: ディレクトリ修正

--- apps/list_sample_app/lib/app.dart
+++ apps/list_sample_app/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:core_model/src/build_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
--- packages/core/model/lib/src/build_config/build_config.dart
+++ packages/core/model/lib/src/build_config/build_config.dart
@@ -1,6 +1,8 @@
 import 'package:core_model/build_config.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'build_config.g.dart';
+
 @Riverpod(keepAlive: true)
 BuildConfig buildConfig(ref) {
   throw UnimplementedError('buildConfig');
